b90349fa4bcd1fa7e5e2345c023da25c56b8f51e
JxJxxJxJ <jcruzartico@gmail.com>
2023-02-25 11:01:42 -0300

    add in_align snippets

---
+++ .config/lvim/luasnippets/tex/in_align.lua
@@ -0,0 +1,28 @@
+---@diagnostic disable: undefined-global
+
+return {
+
+  -- align (note: it has to be used in text and without $$, it already is in mathmode)
+  s({ trig = 'align', regTrig = true, wordTrig = false, snippetType = "autosnippet" },
+    fmta(
+      [[
+\begin{align*}
+    <> &= <> \\
+     &= <> \\
+\end{align*}
+
+      ]],
+      {
+        i(1),
+        i(2),
+        i(3),
+      }
+    ),
+    { condition = in_text }
+  ),
+  -- for quick &=
+  s("alii", {
+    t("&= "),
+    i(1),
+  }, { condition = in_text and begins_line }),
+}
