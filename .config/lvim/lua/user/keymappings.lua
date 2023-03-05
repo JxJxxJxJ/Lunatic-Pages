14fb8c5c59c72d33695fb857ba7cda1f698f1130
JxJxxJxJ <jcruzartico@gmail.com>
2023-03-04 21:08:30 -0300

    added keymaps

--- .config/lvim/lua/user/keymappings.lua
+++ .config/lvim/lua/user/keymappings.lua
@@ -9,4 +9,33 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- override a default keymapping
 -- lvim.keys.normal_mode["<C-q>"] = ":q<cr>" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
 
+-- Just for magic workflow
 
+-- Insert mode mapping for creating figures
+-- lvim.keys.insert_mode["<C-f>"] = '<Esc>:silent exec "!inkscape-figures create \\""..vim.fn.getline(".").."\\" \\""..vim.b.vimtex.root.."/figures/\\""<CR><CR>:w<CR>'
+-- lvim.keys.insert_mode["<C-f>"] = {
+--   '<Esc>:silent exec "!inkscape-figures create \\""..vim.fn.getline(".").."\\" \\""..vim.b.vimtex.root.."/figures/\\""<CR><CR>:w<CR>',
+--   {noremap = true}
+-- }
+-- vim.api.nvim_set_keymap('i', '<C-f>',
+--   '<Esc>:silent exec "!inkscape-figures create \\""..vim.fn.getline(".").."\\" \\""..vim.b.vimtex.root.."/figures/\\""<CR><CR>:w<CR>'
+--   , { noremap = true })
+
+-- Normal mode mapping for editing figures
+-- vim.api.nvim_set_keymap('n', '<C-f>',
+--   ':silent exec "!inkscape-figures edit \\""..vim.b.vimtex.root.."/figures/\\" > /dev/null 2>&1 &"<CR><CR>:redraw!<CR>',
+--   { noremap = true })
+
+
+
+-- vim.keymap.set('i', '<C-f>',
+--   [[<Esc>: silent exec ".!inkscape-figures create \\""..vim.fn.getline(".").."\\" \\""..vim.b.vimtex.root.."/figures/\\""..<CR><CR>:w<CR>]]
+--   , { noremap = true, silent = true })
+-- vim.keymap.set('n', '<C-f>',
+--   [[: silent exec "!inkscape-figures edit \\""..vim.b.vimtex.root.."/figures/\\"" > /dev/null 2>&1 &"<CR><CR>:redraw!<CR>]]
+--   , { noremap = true, silent = true })
+
+vim.keymap.set('i', '<C-f>',
+  [[ <Esc>: silent exec '.!inkscape-figures create "'.getline('.').'" "'.b:vimtex.root.'/figures/"'<CR><CR>:w<CR>]])
+
+vim.cmd [[nnoremap <C-f> : silent exec '!inkscape-figures edit "'.b:vimtex.root.'/figures/" > /dev/null 2>&1 &'<CR><CR>:redraw!<CR>]]
